eed6f3fb70b0cc4dfa73f210936e672da8e22b7d
Cannon Mallory <56271516+malloc3@users.noreply.github.com>
2020-08-06 16:44:33 -0700

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,107 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "My Workflow",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/malloc3/YG_Harmonization/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+      {
+
+        category: "Libraries",
+
+        contents: [
+
+          
+            {
+              name: 'BiotekPlateReaderCalibration',
+              path: 'libraries/BiotekPlateReaderCalibration' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'HighThroughput_Lib',
+              path: 'libraries/HighThroughput_Lib' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'PlateReaderMethods',
+              path: 'libraries/PlateReaderMethods' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'SynchronizationLib',
+              path: 'libraries/SynchronizationLib' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'Upload_PlateReader_Data',
+              path: 'libraries/Upload_PlateReader_Data' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'YG_Controls',
+              path: 'libraries/YG_Controls' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'YG_Measure_OD_GFP',
+              path: 'libraries/YG_Measure_OD_GFP' + '.html',
+              type: "local-webpage"
+            },
+          
+
+        ]
+
+    },
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+        ]
+      }
+    
+
+  ]
+
+};
